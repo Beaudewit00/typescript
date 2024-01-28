58cee30c49c97848666cb34516d685ec7ee59fb8
Beau de Wit <beaudewit00@gmail.com>
2024-01-28 12:49:15 +0100

    Setup test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 document.querySelector(".update-profile-btn")?.addEventListener("click", () => {
     alert("Profile updated!");
 });
+
+function setup(): void {
+    document.querySelector(".username").innerHTML = "Frank Visser";
+}
+
+setup();
